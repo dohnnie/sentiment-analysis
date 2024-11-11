3dbb23ea3d5fc40d0725ba963219a9606b7768dc
Johnny Ngo <jtn.tria@gmail.com>
2024-11-11 17:53:56 -0600

    Added debug print lines

--- src/file-pp.py
+++ src/file-pp.py
@@ -10,15 +10,17 @@ bd_df = pd.read_csv(bd_file)
 
 # Merging the data sets
 books = pd.merge(br_df, bd_df, on = 'Title')
-
+print("Datasets merged!")
 # Pre-Processing steps to remove duplicate values and empty values
 # Only using relevant columns
 df = books[ ['Title', 'review/score', 'review/text', 'authors', 'categories', 'ratingsCount']]
 df.drop_duplicates(inplace=True)
+print("Duplicate values dropped!")
 df.dropna(inplace=True)
-
+print("Null values dropped")
 # Removing brackets and colons from authors name
 df['authors'] = df['authors'].str.extract(r'\'(.*)\'')
-
+print("Brackets and colons removed from authors")
 # Removing brackets and colons from categories
 df['categories'] = df['categories'].str.extract(r'\'(.*)\'')
+print("Brackets and colons removed from categories")
